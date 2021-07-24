99a2e6c1c2f027ac6de64f47ac495b67f0a619d0
haiphung1501 <haiphung15012002@gmail.com>
2021-07-24 20:01:32 +0700

    Changes

--- Demo_Server/Demo_Server.cpp
+++ Demo_Server/Demo_Server.cpp
@@ -1,19 +1,64 @@
-﻿// Demo_Server.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "Demo_Server.h"
 #include "afxsock.h"
+#include <string>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
+using namespace std;
 
 // The one and only application object
 
 CWinApp theApp;
 
+bool checkLogin(string user, string pass) {
+	ifstream ifs("login.txt");
+	if (ifs.fail()) {
+		cout << "Error in Opening! File Not Found!!" << endl;
+		cout << "\n***Press Enter to continue";
+		return 0;
+	}
+	string _user, _pass;
+	while (!ifs.eof()) {
+		getline(cin, _user, '-');
+		getline(cin, _pass);
+		if (_user == user && _pass == pass)
+		{
+			ifs.close();
+			return 1;
+		}
+	}
+	ifs.close();
+	return 0; // wrong password or loi~ gi do
+}
+bool Register(string user, string pass) {
+	ifstream ifs("login.txt");
+	if (ifs.fail()) {
+		cout << "Error in Opening! File Not Found!!" << endl;
+		cout << "\n***Press Enter to continue";
+		return 0;
+	}
+	while (!ifs.eof()) {
+		string _user, _pass;
+		getline(ifs, _user, '-');
+		getline(ifs, _pass);
+		if (_user == user)
+		{
+			cout << "Username has been used";
+			ifs.close();
+			return 0;
+		}
+	}
+	ifs.close();
+
+	ofstream ofs("login.txt", ios::app);
+
+	ofs << "\n" << user << "-" << pass;
+	ofs.close();
+	return 1;
+}
 DWORD WINAPI function_cal(LPVOID arg)
 {
 	SOCKET* hConnected = (SOCKET*)arg;
@@ -30,6 +75,7 @@ DWORD WINAPI function_cal(LPVOID arg)
 		cout << "1. Dang nhap: " << endl;
 		cout << "2. Dang ki: " << endl;
 		cout << "Nhap: ";
+		cin >> mode;
 		switch (mode)
 		{
 		case 1:
@@ -37,7 +83,6 @@ DWORD WINAPI function_cal(LPVOID arg)
 			string id, pass;
 			cout << "Tai khoan: ";
 			getline(cin, s);
-
 		}
 		}
 
@@ -66,29 +111,37 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 		{
 			// TODO: code your application's behavior here.
 			AfxSocketInit(NULL);
-			CSocket server, s;
+			CSocket server;
 			DWORD threadID;
 			HANDLE threadStatus;
-			int port;
-			cout << "Nhap port muon host: ";
-			cin >> port;
-			cin.ignore();
-			server.Create(port);
-			do {
-				cout << "So client ket noi: ";
-				int clients;
-				cin >> clients;
-				cin.ignore();
-				server.Listen(clients);
-				server.Accept(s);
+			int port = 1234;
+			int clients = 1;
+			char sAdd[] = "127.0.0.1";
+			//cout << "Enter port to host: ";
+			//cin >> port;
+			//cin.ignore();
+			//cout << "Enter IP to host: ";
+			//cin.getline(sAdd, 100);
+			//cout << "Enter number of Client(s): ";
+			//cin >> clients;
+			CSocket* s = new CSocket[clients];
+			server.Create(port, 1, CA2W(sAdd));
+			for (int i = 0; i < clients; ++i) {
+				cout << "Server is Listening from Clients\n";
+				server.Listen();
+				server.Accept(s[i]);
+				cout << "Accepted Client " << i + 1 << endl;
 				//Khoi tao con tro Socket
 				SOCKET* hConnected = new SOCKET();
 				//Chuyá»ƒn Ä‘á»i CSocket thanh Socket
-				*hConnected = s.Detach();
+				*hConnected = s[i].Detach();
 				//Khoi tao thread tuong ung voi moi client Connect vao server.
 				//Nhu vay moi client se doc lap nhau, khong phai cho doi tung client xu ly rieng
 				threadStatus = CreateThread(NULL, 0, function_cal, hConnected, 0, &threadID);
-			} while (1);
+				s[i].Attach(*hConnected);
+			}
+
+			server.Close();
 		}
 	}
 	else
@@ -99,5 +152,7 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 	}
 
 	return nRetCode;
+
+	//TEST
 }
 
